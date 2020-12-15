bbf6417964481cc1da9e2c5a0080e9692e26961c
suyesh0 <34532489+suyesh0@users.noreply.github.com>
2020-12-15 17:48:36 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start /var/www/server.js -i 0 --name \"node-app\""; } | crontab -
-sudo pm2 stop node-app
+#crontab -l | { cat; echo "@reboot pm2 start /var/www/server.js -i 0 --name \"node-app\""; } | crontab -
+#sudo pm2 stop node-app
 # actually start the server
-sudo pm2 start /var/www/server.js -i 0 --name "node-app"
+#sudo pm2 start /var/www/server.js -i 0 --name "node-app"
+cd /opt/back-end
+sudo npm install
+sudo pm2 start server.js
 
