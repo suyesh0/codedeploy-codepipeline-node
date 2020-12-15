063cfe88be90a4a78b1272443998f42efb60b55c
suyesh0 <34532489+suyesh0@users.noreply.github.com>
2020-12-15 16:39:50 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -12,3 +12,24 @@ sudo apt-get install -y nodejs
 # install pm2 to restart node app
 # install pm2 with npm
 sudo npm install -g pm2
+sudo apt-get install -y nginx
+sudo systemctl start nginx
+
+# set mongodb to start automatically on system startup
+sudo systemctl enable nginx
+# import mongodb 4.0 public gpg key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+
+# create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+# reload local package database
+sudo apt-get update
+
+# install the latest version of mongodb
+sudo apt-get install -y mongodb-org
+# start mongodb
+sudo systemctl start mongod
+
+# set mongodb to start automatically on system startup
+sudo systemctl enable mongod
