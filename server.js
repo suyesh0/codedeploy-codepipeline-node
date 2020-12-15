62f376733f414a55079e32cb550f6fae26b881eb
suyesh0 <34532489+suyesh0@users.noreply.github.com>
2020-12-15 16:56:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const port = process.env.PORT || 80
-require('http')
-  .createServer((req, res) => {
-    console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
-    res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.write('<p>Buenos dias Montevideo</p>')
-    res.write('<p>This is Saturday. Bla blah blah</p>')
-    res.end('<h1>Hello World from amazing Node University course AWS Intermediate</h1>')
-  })
-  .listen(port, (error)=>{
-    console.log(`server is running on ${port}`)
-  })
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
